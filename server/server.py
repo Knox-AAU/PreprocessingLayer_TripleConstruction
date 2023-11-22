62ed5f1801de227d33c88e52b0f1eed15d560883
unknown <Asbjoernjc@gmail.com>
2023-11-22 10:43:56 +0100

    Added an additional test and added some exception handling for the server's endpoint

--- server/server.py
+++ server/server.py
@@ -3,32 +3,49 @@ import json
 
 class PreProcessingHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        message = ""
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        post_data = post_data.decode('utf-8')
-        post_json = json.loads(post_data)
+        post_content = {"post_data": self.rfile.read(content_length), "post_json": {}}
+
+        if not self.handled_request_body(post_content):
+            return
 
         if self.path == '/tripleconstruction':
-            self.send_header('Content-type','text/html')
-            self.end_headers()
             try:
-                for f in post_json:
+                for f in post_content['post_json']:
                     for sentence in f["sentences"]:
                         for em in sentence["entityMentions"]:
                             print(em["name"])
-                message = "POST to group-c data: " + post_data
                 self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                message = "POST to group-c data: " + post_content['post_data']
             except:
-                self.send_response(422)
-            
+                self.wrongly_formatted_request_response()
+                return
         else:
             self.send_response(404)
             self.send_header('Content-type','text/html')
             self.end_headers()
-
             message = "Invalid endpoint"
 
         self.wfile.write(bytes(message, "utf8"))
+
+    def wrongly_formatted_request_response(self):
+        self.send_response(422)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes("The request body was incorrectly formatted", "utf8"))
+
+    def handled_request_body(self, post_content):
+        try:
+            post_content['post_data'] = post_content['post_data'].decode('utf-8')
+            post_content['post_json'] = json.loads(post_content['post_data'])
+            return True
+        except:
+            self.wrongly_formatted_request_response()
+            return (False)
+
 
 if __name__ == '__main__':
     with HTTPServer(('', 8000), PreProcessingHandler) as server:

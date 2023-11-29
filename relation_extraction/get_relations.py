113d93b2a9b2263ae6bc69a7d23852d1aa43d882
AndersToft20 <atoft20@student.aau.dk>
2023-11-29 14:24:29 +0100

    more tests

--- relation_extraction/get_relations.py
+++ relation_extraction/get_relations.py
@@ -8,15 +8,14 @@ def extract_specific_relations_offline():
     relations = set()
     with open(ontology_file_path, 'r', encoding='utf-8', errors='ignore') as file:
         lines = file.readlines()
-        i = 0
         for i, line in enumerate(lines):
             line = line.strip()
             # Check if the line starts with a colon and the next lines contain the specified pattern
-            if line.startswith(":") and i+1 < len(lines) and "a rdf:Property, owl:ObjectProperty ;" in lines[i+1]:
+            if line.startswith(":") and i+1 <= len(lines) and "a rdf:Property, owl:ObjectProperty ;" in lines[i+1]:
                 relation = line.split()[0]  # Extracting the relation name
                 relation = relation[1:] # Remove colon
                 relations.add(relation)
-            i += 1
+            
             
     return sorted(relations) 
 
@@ -40,4 +39,5 @@ def extract_specific_relations():
         
     return relations
 
-extract_specific_relations()
+if __name__ == "__main__":
+    extract_specific_relations()    

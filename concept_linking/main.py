1b40e308e860ad5363d73f487b6859dac021f897
casparejensen <casparejensen@gmail.com>
2023-12-07 18:55:42 +0100

    Integrated StringComparison solution

--- concept_linking/main.py
+++ concept_linking/main.py
@@ -1,6 +1,7 @@
 import os
 from concept_linking.solutions.PromptEngineering.main import perform_entity_type_classification
 from concept_linking.solutions.UntrainedSpacy.main import untrainedSpacySolution
+from concept_linking.solutions.StringComparison.main import stringComparisonSolution
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "./"))
 
@@ -10,12 +11,12 @@ def entity_type_classification(input_data):
     # Remove the comment sign from the solution you want to run as main in the KNOX Pipeline.
 
     # String Comparison
-    # print(f'{msg} "StringComparison solution"')
+    stringComparisonSolution(input_data)
 
     # Untrained Spacy
     #untrainedSpacySolution(input_data)
 
     # PromptEngineering
-    perform_entity_type_classification(input_data)
+    #perform_entity_type_classification(input_data)
 
     # Machine Learning
